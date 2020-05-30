d2f8846b1e9c9612f89eaaabbbe08e1198aa86f8
gergely <kiss-gergely@outlook.com>
2020-05-30 08:07:11 +0100

    first commit

---
+++ src/main/java/uk/gergely/kiss/bdd_intelij/BddIntelijApplication.java
@@ -0,0 +1,13 @@
+package uk.gergely.kiss.bdd_intelij;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BddIntelijApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BddIntelijApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uk/gergely/kiss/bdd_intelij/BddIntelijApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.gergely.kiss.bdd_intelij;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BddIntelijApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
